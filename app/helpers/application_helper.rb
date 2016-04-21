85f66ddcf2a48095738b1d7a72c9a8667b558325
Luc Lussier <luc.lussier@gmail.com>
2016-04-20 19:33:02 -0700

    override DoorMat default views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,12 @@ module ApplicationHelper
     }[flash_type.to_sym] || flash_type.to_s
   end
 
+  def session_type(session)
+    {
+        :public_computer => 'Public Computer',
+        :private_computer => 'Private Computer',
+        :remember_me => 'Private Computer / Remember Me'
+    }[session.rating.to_sym]
+  end
+
 end
